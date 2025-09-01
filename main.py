588fc4b6bf9e12801e3b0a2bf33f4c08c6a83f92
jonatan ocampo morales <jonaocampo16@gmail.com>
2025-09-01 18:33:32 -0500

    Colisiones agregadas

--- main.py
+++ main.py
@@ -13,19 +13,28 @@ class Ball:
         self.x = x
         self.y = y
         self.w = 5
+        self.radius = 6
         self.dx = random.uniform(-1,1)
         self.dy = random.uniform(-1,1)
         self.color = "blue"
         self.speed = 5
     
     def draw(self):
-        pygame.draw.circle(window, self.color, (self.x, self.y), 7, self.w)
+        pygame.draw.circle(window, self.color, (self.x, self.y), self.radius, self.w)
 
     def move(self):
         self.x += self.dx * self.speed
         self.y += self.dy * self.speed
-        
 
+        if self.y <=0 or self.y >= 400:
+            self.dy *= -1
+
+    def get_react(self):
+        return pygame.Rect(self.x - self.radius, self.y-self.radius, self.radius*2, self.radius*2)
+        
+    def bounce_padel(self):
+        self.dx *= -1
+    
         
 class Padel:
     def __init__(self, x, y, w, h):
@@ -38,8 +47,19 @@ class Padel:
     def draw(self):
         pygame.draw.rect(window, "white", (self.x, self.y, self.w, self.h))
 
-p1 = Padel(10, 10, 30, 150)
-p2 = Padel(360, 10, 30, 150)
+    def get_react(self):
+        return pygame.Rect(self.x, self.y, self.w, self.h)
+    
+    def check_collisions(self, ball):
+        return self.get_react().colliderect(ball.get_react())
+
+    def update(self, ball):
+        self.draw()
+        if self.check_collisions(ball):
+            ball.bounce_padel()
+
+p1 = Padel(20, 10, 10, 140)
+p2 = Padel(370, 10, 10, 140)
 ball = Ball(100, 100)
 
 def movimientos(padel1: Padel, padel2: Padel):
@@ -58,8 +78,8 @@ def movimientos(padel1: Padel, padel2: Padel):
 
 while runnin:
     clock.tick(FPS)
-    p1.draw()
-    p2.draw()
+    p1.update(ball)
+    p2.update(ball)
     ball.draw()
     ball.move()
     for envent in pygame.event.get():
